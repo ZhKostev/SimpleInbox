f6f93d12e1c9be79d1b0ef699b61c2485947e113
Zhenya Kostev <zh.kostev@gmail.com>
2012-02-05 14:42:26 +0300

    generator creating

--- lib/generators/simple_inbox/install/install_generator.rb
+++ lib/generators/simple_inbox/install/install_generator.rb
@@ -1,13 +1,13 @@
 module SimpleInbox
 
-  class InstallGenerator < Rails::Generators::NamedBase
+  class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
     desc "Copies files to application"
 
     def copy_files
-      copy_file '/app/controllers/messages_controller.rb', '/app/controllers/messages_controller.rb'
-      copy_file '/app/models/message.rb', '/app/models/message.rb'
-      copy_file '/app/models/message_copy.rb', '/app/models/message_copy.rb'
+      copy_file 'app/controllers/messages_controller.rb', '/app/controllers/messages_controller.rb'
+      copy_file 'app/models/message.rb', '/app/models/message.rb'
+      copy_file 'app/models/message_copy.rb', '/app/models/message_copy.rb'
     end
   end
 end
