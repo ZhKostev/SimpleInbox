fd25f897d82110cf72936a91ad7137e02875045f
Zhenya Kostev <zh.kostev@gmail.com>
2012-02-05 16:17:57 +0300

    migration

--- lib/generators/simple_inbox/install/install_generator.rb
+++ lib/generators/simple_inbox/install/install_generator.rb
@@ -8,7 +8,8 @@ module SimpleInbox
       copy_file 'app/controllers/messages_controller.rb', 'app/controllers/messages_controller.rb'
       copy_file 'app/models/message.rb', 'app/models/message.rb'
       copy_file 'app/models/message_copy.rb', 'app/models/message_copy.rb'
-      copy_file 'db/migrate/message_install_migration.rb', "db/migrate/#{Time.now.stftime(%w(%Y%m%d%H%M%s))}message_install_migration.rb"
+      migration_name = "db/migrate/" + Time.now.strftime("%Y%m%d%H%M%S") + "_message_install_migration.rb"
+      copy_file 'db/migrate/message_install_migration.rb', "#{migration_name}"
     end
   end
 end
